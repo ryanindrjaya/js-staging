ae551f0afdbf0d2996c65c86145656b31cafd59e
Ryan Indrajaya <sryan062@gmail.com>
2022-11-02 12:38:20 +0700

    Update: user location

--- components/Form/Locations.js
+++ components/Form/Locations.js
@@ -1,10 +1,22 @@
 import { Form, Select } from "antd";
 
+const formatInitialValue = (arr) => {
+  if (arr?.length > 0) {
+    return arr?.map((item) => {
+      return {
+        label: item.name,
+        value: item.id,
+      };
+    });
+  }
+};
+
 export default function Locations({ data, onSelect, initialValue, errMsg, required = false }) {
+  const formattedInitialValue = formatInitialValue(initialValue);
   return (
     <div className="w-full md:w-2/3 mb-2 md:mb-0">
       <Form.Item
-        initialValue={initialValue}
+        initialValue={formattedInitialValue}
         name="locations"
         className="w-1/1"
         rules={[
