012c61a1b608b2065f7863064761bf63c0f48ae5
Ryan Indrajaya <sryan062@gmail.com>
2022-11-03 22:37:26 +0700

    add skeleton loading

---
+++ components/Skeleton/TitleSkeleton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TitleSkeleton({ titleText }) {
+  return (
+    <div className="mb-3 animate-pulse">
+      <h5 className=" border-l-4 border-gray-400">
+        <div className="ml-4 w-24 h-6 rounded-full bg-gray-200"></div>{" "}
+      </h5>
+    </div>
+  );
+}
