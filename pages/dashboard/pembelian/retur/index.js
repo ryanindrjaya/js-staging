1f9e47a7a52c2e052fef0d1b65400d3a30d2a9d6
Yudi Ananta <yunata.iona@gmail.com>
2022-12-14 12:31:06 +0700

    fix pagination

--- pages/dashboard/pembelian/retur/index.js
+++ pages/dashboard/pembelian/retur/index.js
@@ -115,13 +115,26 @@ function Retur({ props }) {
     }
   };
 
+
+  const filterDuplicateData = (arr) => {
+    const seen = new Set();
+
+    const filteredArr = arr.filter((el) => {
+      const duplicate = seen.has(el.id);
+      seen.add(el.id);
+      return !duplicate;
+    });
+
+    return filteredArr;
+  };
+
   const handleCancel = async (id, row) => {
     onChangeStatus("Dibatalkan", row);
   };
 
   const onChangeStatus = (status, row) => {
     row.attributes.status = status;
-    handleChangeStatus(row, row.id);
+    // handleChangeStatus(row, row.id);
   };
 
   const openNotificationWithIcon = (type, title, message) => {
