43d4a99974bea6a98c5d954b414ba9533deaa358
Yudian00 <yunata.iona@gmail.com>
2023-03-28 08:43:33 +0700

    revamp inventory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#036b82",
+      },
+    },
   },
   plugins: [],
   screens: {
     // ml stands for 'mobile landscape'
     ml: {
-      raw: '(min-width: 480px) and (max-width: 960px) and (max-height: 480px)',
+      raw: "(min-width: 480px) and (max-width: 960px) and (max-height: 480px)",
     },
   },
 };
