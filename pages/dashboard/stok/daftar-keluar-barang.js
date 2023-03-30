289976c1ae0d58913c5a11cfab75b1f6e5a287a8
Ryan Indrajaya <sryan062@gmail.com>
2023-03-30 07:02:19 +0700

    fix minor bug

--- pages/dashboard/stok/daftar-keluar-barang.js
+++ pages/dashboard/stok/daftar-keluar-barang.js
@@ -293,7 +293,7 @@ export default function daftarKeluarBarang({ companyOptions }) {
         }
 
         if (row.status === "Selesai") {
-          maxQty = null;
+          maxQty = 999;
         }
 
         return (
