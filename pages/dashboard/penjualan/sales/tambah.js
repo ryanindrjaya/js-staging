0b2ec3906cd5f61a3e9e86d3d05f74ea02307469
Achmad Fauzi S <fsaksenata@gmail.com>
2023-05-16 15:11:18 +0700

    customer fix bug

--- pages/dashboard/penjualan/sales/tambah.js
+++ pages/dashboard/penjualan/sales/tambah.js
@@ -751,7 +751,7 @@ function Toko({ props }) {
                       },
                     ]}
                   >
-                    <Customer onChangeCustomer={setCustomer} />
+                    <Customer onChangeCustomer={setCustomer} page={"SALES"} />
                   </Form.Item>
                 </div>
                 <div className="w-full md:w-1/3 px-3 mb-2">
