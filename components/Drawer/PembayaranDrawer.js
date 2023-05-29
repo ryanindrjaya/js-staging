96f0b9a0e6ae17a63c996b8aaaf208d8f1406949
Ryan Indrajaya <sryan062@gmail.com>
2023-05-30 04:11:42 +0800

    fix

--- components/Drawer/PembayaranDrawer.js
+++ components/Drawer/PembayaranDrawer.js
@@ -1,8 +1,5 @@
 import { Button, Drawer, InputNumber, Popconfirm, Select } from "antd";
-import {
-  CreateStorePaymenWithoutUpdate,
-  updateTransaction,
-} from "../../library/functions/createStorePayment";
+import { CreateStorePaymenWithoutUpdate, updateTransaction } from "../../library/functions/createStorePayment";
 import React, { useState } from "react";
 import { createInventoryFromPenjualan } from "../../library/functions/createInventory";
 
@@ -25,14 +22,7 @@ function PembayaranDrawer({ openDrawer, onCloseDrawer, record, reloadPage }) {
         if (option && nominal) {
           const {
             data: { id },
-          } = await CreateStorePaymenWithoutUpdate(
-            total,
-            totalCharge,
-            nominal,
-            option,
-            "Pembayaran",
-            id
-          );
+          } = await CreateStorePaymenWithoutUpdate(total, totalCharge, nominal, option, "Pembayaran", id);
           console.log("response api", id);
           listPaymentId.push(id);
         }
@@ -56,12 +46,20 @@ function PembayaranDrawer({ openDrawer, onCloseDrawer, record, reloadPage }) {
     minimumFractionDigits: 2,
   });
 
-  const [values, setValues] = useState(
-    Array.from({ length: 3 }, () => ({
-      option: "",
+  const [values, setValues] = useState([
+    {
+      option: "CASH",
       nominal: 0,
-    }))
-  );
+    },
+    {
+      option: "TRANSFER BANK",
+      nominal: 0,
+    },
+    {
+      option: "KARTU KREDIT",
+      nominal: 0,
+    },
+  ]);
 
   const handleDrawerClose = () => {
     setValues(
@@ -88,12 +86,7 @@ function PembayaranDrawer({ openDrawer, onCloseDrawer, record, reloadPage }) {
 
     return (
       <div key={index} className="mt-5">
-        <Select
-          style={{ width: 120 }}
-          className="mr-2"
-          value={values[index].option}
-          onChange={handleChangeOption}
-        >
+        <Select style={{ width: 120 }} className="mr-2" value={values[index].option} onChange={handleChangeOption}>
           <Option value="CASH">Cash</Option>
           <Option value="TRANSFER BANK">Transfer Bank</Option>
           <Option value="KARTU KREDIT">Kartu Kredit</Option>
@@ -101,9 +94,7 @@ function PembayaranDrawer({ openDrawer, onCloseDrawer, record, reloadPage }) {
         <InputNumber
           placeholder="Masukan Nominal"
           min={0}
-          formatter={(value) =>
-            `Rp ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-          }
+          formatter={(value) => `Rp ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
           parser={(value) => value.replace(/Rp\s?|(,*)/g, "")}
           style={{ width: 150 }}
           value={values[index].nominal}
@@ -116,36 +107,20 @@ function PembayaranDrawer({ openDrawer, onCloseDrawer, record, reloadPage }) {
 
   const totalInputValue = values.reduce((acc, cur) => acc + cur.nominal, 0);
   const totalAfterInput = data?.total - totalInputValue; // sisa pembayaran
-  const totalCharge =
-    totalAfterInput <= 0 ? (data?.total - totalInputValue) * -1 : 0;
+  const totalCharge = totalAfterInput <= 0 ? (data?.total - totalInputValue) * -1 : 0;
   return (
-    <Drawer
-      title={`Pembayaran Lainnya`}
-      placement="right"
-      size="default"
-      onClose={handleDrawerClose}
-      open={openDrawer}
-    >
+    <Drawer title={`Pembayaran Lainnya`} placement="right" size="default" onClose={handleDrawerClose} open={openDrawer}>
       <p>No.Faktur : {data?.faktur ?? ""}</p>
       <p>Nama Customer : {data?.customer_name ?? ""}</p>
       <p>Total Harga : {formatter.format(data?.total ?? 0)}</p>
 
       {Array.from({ length: 3 }, (_, i) => renderInput(i))}
 
-      <p className="mt-5">
-        Sisa Pembayaran :{" "}
-        {formatter.format(totalAfterInput < 0 ? 0 : totalAfterInput)}
-      </p>
-      <p className="mt-2 font-medium text-red-400">
-        Pengembalian : {formatter.format(totalCharge)}
-      </p>
+      <p className="mt-5">Sisa Pembayaran : {formatter.format(totalAfterInput < 0 ? 0 : totalAfterInput)}</p>
+      <p className="mt-2 font-medium text-red-400">Pengembalian : {formatter.format(totalCharge)}</p>
 
       <Popconfirm
-        title={
-          "Pembayaran akan dilakukan sebesar " +
-          formatter.format(totalInputValue) +
-          ". Lanjutkan?"
-        }
+        title={"Pembayaran akan dilakukan sebesar " + formatter.format(totalInputValue) + ". Lanjutkan?"}
         onConfirm={() => confirm()}
         onCancel={cancel}
         okButtonProps={{
